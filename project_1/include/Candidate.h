4521ab911bd82678782a49b527cb1d7158de10b5
Anthony <phanx211@umn.edu>
2018-03-06 17:56:46 -0600

    Filled out candidate.h

--- project_1/include/Candidate.h
+++ project_1/include/Candidate.h
@@ -5,3 +5,31 @@
  * Original Authors : Maxwell Dahl, Sanjana Jonnalagadda, Anthony Phan,
                       Ronny Yogiswara
  ******************************************************************************/
+
+/*******************************************************************************
+* Includes
+******************************************************************************/
+#include <string>
+#include <iostream>
+#include "include/BallotList.h"
+#include "include/Ballot.h"
+
+class Candidate {
+	public:
+		//Constructors
+		Candidate();
+		Candidate(std::string name, int num_ballots, BallotList votes);
+
+		// Getters/Setters
+		std::string get_name() {return name;}
+		int get_num_ballots() {return num_ballots;}
+		BallotList get_votes() {return votes;}
+
+		void set_name(std::string name) {name = name;}
+		void set_num_ballots(int num) {num_ballots = num;}
+		void set_votes(BallotList votes) {votes = votes;}
+	private:
+		std::string name;
+		int num_ballots;
+		BallotList votes; 		
+}

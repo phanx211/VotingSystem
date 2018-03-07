420b8016109ee45a98885812661c45cfc522ff04
Ronny Yogiswara <yogis002@umn.edu>
2018-03-06 18:15:24 -0600

    Added definitions for CandidateList

--- project_1/include/CandidateList.h
+++ project_1/include/CandidateList.h
@@ -5,3 +5,31 @@
  * Original Authors : Maxwell Dahl, Sanjana Jonnalagadda, Anthony Phan,
                       Ronny Yogiswara
  ******************************************************************************/
+
+ /*******************************************************************************
+  * Includes
+  ******************************************************************************/
+ #include <cmath>
+ #include <iostream>
+ #include <vector>
+ #include <string>
+
+ #include "include/Candidate.h"
+ #include "include/Ballotlist.h"
+
+ /*******************************************************************************
+  * Class Definitions
+  ******************************************************************************/
+
+class CandidateList {
+public:
+  void Add(Candidate c){};
+  void Remove(std::string name){};
+  Candidate ReturnLoser();
+  int ListSize();
+
+  std::vector<Candidate> get_candidate_list(){return candidate_list};
+  void set_candidate_list(std::vector<Candidate> cl) {candidate_list = cl;}
+private:
+  std::vector<Candidate> candidate_list;
+}

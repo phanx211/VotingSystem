32f8dc12a5e65ef9099486fd6dfe925467bc4da5
Maxwell Dahl <dahlx516@umn.edu>
2018-03-06 18:04:15 -0600

    Adding Ballot header and implementation

--- project_1/include/Ballot.h
+++ project_1/include/Ballot.h
@@ -5,3 +5,39 @@
  * Original Authors : Maxwell Dahl, Sanjana Jonnalagadda, Anthony Phan,
                       Ronny Yogiswara
  ******************************************************************************/
+
+#ifndef PROJECT_INCLUDE_BALLOT_H_
+#define PROJECT_INCLUDE_BALLOT_H_
+
+/*!******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include <iostream>
+#include <string>
+#include <vector>
+
+/*!******************************************************************************
+ * Class Definitions
+ ******************************************************************************/
+
+class Ballot {
+	public:
+		//Constructors
+		Ballot();
+		Ballot(int ballot_no, std::string vote_type, int<> votes);
+
+		//Getters/Setters
+		int get_ballot_no() {return ballot_no;}
+		void set_ballot_no(int ballot_no) {ballot_no = ballot_no;}
+
+		std::string get_vote_type() {return vote_type;}
+		void set_vote_type(std::string vote_type) {vote_type = vote_type;}
+
+		int<> get_votes() {return votes;}
+		void set_votes(int<> votes) {votes = votes;}
+		
+	private:
+		int ballot_no;
+		std::string vote_type;
+		int<> votes;
+}
--- project_1/src/Ballot.cc
+++ project_1/src/Ballot.cc
@@ -1,1 +1,14 @@
-#include Ballot.h
+#include <string>
+#include "include/Ballot.h"
+
+Ballot::Ballot() {
+	ballot_no = NULL;
+	vote_type = NULL;
+	votes = NULL;
+}
+
+Ballot::Ballot(int ballot, std::string type, int<> votes) {
+	ballot_no = ballot;
+	vote_type = type;
+	votes = votes;
+}

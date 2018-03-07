03d46e7f74007248029a488219c17e0bcfce5ed4
Maxwell Dahl <dahlx516@umn.edu>
2018-03-06 18:37:09 -0600

    Adding BallotList.h

--- project_1/include/BallotList.h
+++ project_1/include/BallotList.h
@@ -5,3 +5,39 @@
  * Original Authors : Maxwell Dahl, Sanjana Jonnalagadda, Anthony Phan,
                       Ronny Yogiswara
  ******************************************************************************/
+
+#ifndef PROJECT_INCLUDE_BALLOTLIST_H_
+#define PROJECT_INCLUDE_BALLOTLIST_H_
+
+/*!******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include <iostream>
+#include <string>
+#include <vector>
+#include "include/Ballot.h"
+
+/*!******************************************************************************
+ * Class Definition
+ ******************************************************************************/
+
+class BallotList {
+	public:
+		//Constructors
+		BallotList();
+		BallotList(Ballot<> ballots);
+
+		//Getters/Setters
+		Ballot<> get_ballot_list() {return ballot_list;}
+		void set_ballot_list(Ballot<> ballots) {ballot_list = ballots;}
+
+		//Methods
+		void ShuffleBallots();
+		void ReadFile(std::string file);
+		Ballot RemoveBallot(int voter_no);
+		void AddBallot(Ballot ballot);
+		int Size();
+
+	private:
+		Ballot<> ballot_list;
+}

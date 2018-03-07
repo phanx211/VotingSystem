e09cd064ce1361c0065c800abbd69994bbbce78a
Ronny Yogiswara <yogis002@umn.edu>
2018-03-06 18:35:13 -0600

    Defined methods

--- project_1/src/CandidateList.cc
+++ project_1/src/CandidateList.cc
@@ -1,1 +1,43 @@
-#include CandidateList.h
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "CandidateList.h"
+
+/*******************************************************************************
+ * Class Definitions
+ ******************************************************************************/
+
+CandidateList::CandidateList() {
+  candidate_list = NULL;
+}
+
+void CandidateList::Add(Candidate c) {
+  candidate_list.push_back(c);
+}
+
+Candidate CandidateList::Remove(std::string name) {
+  int i=0;
+  for (Candidate c:candidate_list) {
+    if (c.get_name()!=name) {
+      i++;
+    }
+    else {
+      break;
+    }
+  }
+  candidate_list.erase(myvector.begin()+i);
+
+}
+
+Candidate CandidateList::ReturnLoser(std::string name){
+
+}
+
+int CandidateList::ListSize(){
+  return candidate_list.size();
+}

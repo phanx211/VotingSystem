f23fb251358ca4a054ec61cb1a4046735bc7d78e
Maxwell Dahl <dahlx516@umn.edu>
2018-03-19 13:28:33 -0500

    additions to test files for CXXTEST

--- project_1/tests/BallotList_tests.h
+++ project_1/tests/BallotList_tests.h
@@ -1,3 +1,34 @@
 #include <cxxtest/TestSuite.h>
 #include "Ballot.h"
 #include "BallotList.h"
+
+class BallotListTestSuite : public CxxTest::TestSuite {
+
+  public:
+
+    BallotList bl;
+
+    void ShuffleBallots_test() {
+
+    }
+
+    void RemoveBallot_test() {
+
+    }
+
+    void AddBallot_test() {
+
+    }
+
+    void ListSize_test() {
+
+    }
+
+    void MakeBallot_test() {
+
+    }
+
+    void ReadFile_test() {
+      
+    }
+}
--- project_1/tests/Candidate_tests.h
+++ project_1/tests/Candidate_tests.h
@@ -1,2 +1,20 @@
 #include <cxxtest/TestSuite.h>
 #include "include/Candidate.h"
+
+class CandidateTestSuite : public CxxTest::TestSuite {
+
+  public:
+
+    Candidate *c;
+
+    test_generic_constructor() {
+      c = new Candidate();
+      TS_ASSERT(c);
+      TS_ASSERT(c.get_name() == "");
+      TS_ASSERT(c.get_num_ballots() == 0);
+    }
+
+    test_custom_constructor() {
+
+    }
+}

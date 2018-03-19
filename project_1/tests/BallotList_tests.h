9ad2fa3e7c3833750ded4bed5015fdab663c127f
Maxwell Dahl <dahlx516@umn.edu>
2018-03-19 18:14:21 -0500

    Adding Ballot and Ballotlist tests

--- project_1/tests/BallotList_tests.h
+++ project_1/tests/BallotList_tests.h
@@ -1,4 +1,6 @@
 #include <cxxtest/TestSuite.h>
+#include <cstdlib>
+#include <algorithm>
 #include "Ballot.h"
 #include "BallotList.h"
 
@@ -6,29 +8,81 @@ class BallotListTestSuite : public CxxTest::TestSuite {
 
   public:
 
-    BallotList bl;
-
     void ShuffleBallots_test() {
-
+		BallotList b;
+		vector<int> v;
+		v.push_back(1);
+		v.push_back(2);
+		Ballot b0(1,v);
+		Ballot b1(2,v);
+		Ballot b2(3,v);
+		b.AddBallot(b0);
+		b.AddBallot(b1);
+		b.AddBallot(b2);		
     }
 
     void RemoveBallot_test() {
-
+		BallotList b;
+		vector<int> v;
+		v.push_back(1);
+		v.push_back(2);
+		Ballot b0(1,v);
+		Ballot b1(2,v);
+		Ballot b2(3,v);
+		b.AddBallot(b0);
+		b.AddBallot(b1);
+		b.AddBallot(b2);
     }
 
     void AddBallot_test() {
-
+		BallotList b;
+		vector<int> v;
+		v.push_back(1);
+		v.push_back(2);
+		Ballot b0(1,v);
+		Ballot b1(2,v);
+		Ballot b2(3,v);
+		b.AddBallot(b0);
+		b.AddBallot(b1);
+		b.AddBallot(b2);
     }
 
     void ListSize_test() {
-
+		BallotList b;
+		vector<int> v;
+		v.push_back(1);
+		v.push_back(2);
+		Ballot b0(1,v);
+		Ballot b1(2,v);
+		Ballot b2(3,v);
+		b.AddBallot(b0);
+		b.AddBallot(b1);
+		b.AddBallot(b2);
     }
 
     void MakeBallot_test() {
-
+		BallotList b;
+		vector<int> v;
+		v.push_back(1);
+		v.push_back(2);
+		Ballot b0(1,v);
+		Ballot b1(2,v);
+		Ballot b2(3,v);
+		b.AddBallot(b0);
+		b.AddBallot(b1);
+		b.AddBallot(b2);
     }
 
     void ReadFile_test() {
-      
+		BallotList b;
+		vector<int> v;
+		v.push_back(1);
+		v.push_back(2);
+		Ballot b0(1,v);
+		Ballot b1(2,v);
+		Ballot b2(3,v);
+		b.AddBallot(b0);
+		b.AddBallot(b1);
+		b.AddBallot(b2);
     }
 }
--- project_1/tests/Ballot_tests.h
+++ project_1/tests/Ballot_tests.h
@@ -1,5 +1,5 @@
 #include <cxxtest/TestSuite.h>
-#include "include/Ballot.h"
+#include "../include/Ballot.h"
 
 using namespace std;
 
@@ -7,19 +7,17 @@ class BallotTestSuite : public CxxTest::TestSuite {
 
   public:
 
-    Ballot *b;
-
     void test_generic_constructor() {
-      b = new Ballot();
-      TS_ASSERT(b);
-      TS_ASSERT(b.ballot_no == 0);
+      Ballot b;
+      //TS_ASSERT(b);
+      TS_ASSERT(b.get_ballot_no() == 0);
     }
 
     void test_custom_constructor() {
       vector<int> v;
       v.push_back(1);
       v.push_back(2);
-      b = new Ballot(1,v);
+      Ballot b(1,v);
 
       TS_ASSERT(b.get_ballot_no() == 1);
       TS_ASSERT(b.get_votes()[0] == 1 && b.get_votes()[1] == 2);

95778a7988eade6c25dd0b36a2a5940ef897749d
sanjaJonna <30486815+sanjaJonna@users.noreply.github.com>
2018-03-19 18:25:52 -0500

    Added doxit type comments to Ballot.h

--- project_1/include/Ballot.h
+++ project_1/include/Ballot.h
@@ -17,21 +17,49 @@
 #include <vector>
 
 
-/*!******************************************************************************
+/*******************************************************************************
  * Class Definitions
  ******************************************************************************/
-
+/**
+* @brief An object containing the voting ballots
+*    - The Ballot class will be the main structure to instantiate each voting ballot.
+*    - The Ballot class stores the ballot number, the voting type pertaining to the ballot, and the votes for that ballot candidates
+*/
 class Ballot {
 	public:
-		//Constructors
+		//Constructor
 		Ballot();
+
+		/**
+		* @brief Creates a ballot that contains the ballot number and the voters corresponding votes.
+		*
+		* @param num Takes in the ballot number.
+		*
+		* @param vote Stores a single instance of the ballot number votes.
+		*/
 		Ballot(int num, std::vector<int> vote);
 
 		//Getters/Setters
+
+		/**
+		* @brief Gets the ballot number from the class
+		*/
 		int get_ballot_no() {return ballot_no;}
+
+		/**
+		* @brief Sets the ballot number (b_no) to the local variable (ballot_no) to store the ballot number
+		*
+		* @param b_no Store the ballot number.
+		*/
 		void set_ballot_no(int b_no) {ballot_no = b_no;}
 
 		std::vector<int> get_votes() {return votes;}
+
+		/**
+		* @brief Sets the "vote" vector that contains all the ballot votes to the class variable "votes" vector
+		*
+		* @param vote Contains all the voters votes for that ballot
+		*/
 		void set_votes(std::vector<int> vote) {votes = vote;}
 
 	private:

e409a425bb4c3f0271f6b439e83fcc74d663c88e
Ronny Yogiswara <yogis002@umn.edu>
2018-03-20 15:52:31 -0500

    Fixed test_ListSize

--- project_1/tests/BallotList_tests.h
+++ project_1/tests/BallotList_tests.h
@@ -1,32 +1,40 @@
 #include <cxxtest/TestSuite.h>
 #include <cstdlib>
 #include <algorithm>
-#include "Ballot.h"
-#include "BallotList.h"
+#include "../include/Ballot.h"
+#include "../include/BallotList.h"
+
+using namespace std;
 
 class BallotListTestSuite : public CxxTest::TestSuite {
 
   public:
 
-    void ShuffleBallots_test() {
-		BallotList b;
-		vector<int> v;
-		v.push_back(1);
-		v.push_back(2);
-		Ballot b0(1,v);
-		Ballot b1(2,v);
-		Ballot b2(3,v);
-		b.AddBallot(b0);
-		b.AddBallot(b1);
-		b.AddBallot(b2);
-		BallotList c = b;
-		c.ShuffleBallots();
-		TS_ASSERT(c.get_ballot_list()[0].get_ballot_no() != b.get_ballot_list()[0].get_ballot_no());
-		TS_ASSERT(c.get_ballot_list()[1].get_ballot_no() != b.get_ballot_list()[1].get_ballot_no());
-		TS_ASSERT(c.get_ballot_list()[2].get_ballot_no() != b.get_ballot_list()[2].get_ballot_no());
+    void test_ShuffleBallots() {
+      BallotList b;
+  		vector<int> v;
+  		v.push_back(1);
+  		v.push_back(2);
+  		Ballot b0(1,v);
+  		Ballot b1(2,v);
+  		Ballot b2(3,v);
+  		b.AddBallot(b0);
+  		b.AddBallot(b1);
+  		b.AddBallot(b2);
+  		BallotList c = b;
+      vector<int> original;
+      original.push_back(1);
+      original.push_back(2);
+      original.push_back(3);
+  		c.ShuffleBallots();
+      vector<int> shuffled;
+      shuffled.push_back(b.get_ballot_list()[0].get_ballot_no());
+      shuffled.push_back(b.get_ballot_list()[1].get_ballot_no());
+      shuffled.push_back(b.get_ballot_list()[2].get_ballot_no());
+  		TS_ASSERT(original != shuffled);
     }
 
-    void RemoveBallot_test() {
+    void test_RemoveBallot() {
 		BallotList b;
 		vector<int> v;
 		v.push_back(1);
@@ -43,7 +51,7 @@ class BallotListTestSuite : public CxxTest::TestSuite {
 		}
     }
 
-    void AddBallot_test() {
+    void test_AddBallot() {
 		BallotList b;
 		vector<int> v;
 		v.push_back(1);
@@ -61,7 +69,7 @@ class BallotListTestSuite : public CxxTest::TestSuite {
 		TS_ASSERT(b.get_ballot_list()[3].get_ballot_no() == 4);
     }
 
-    void ListSize_test() {
+    void test_ListSize() {
 		BallotList b;
 		vector<int> v;
 		v.push_back(1);
@@ -75,17 +83,17 @@ class BallotListTestSuite : public CxxTest::TestSuite {
 		TS_ASSERT(b.ListSize() == 3);
     }
 
-    void MakeBallot_test() {
+    void test_MakeBallot() {
 		BallotList b;
 		b.MakeBallot("1,2,3");
 		TS_ASSERT(b.ListSize() == 1);
 		TS_ASSERT(b.get_ballot_list()[0].get_votes()[0] == 1);
 		TS_ASSERT(b.get_ballot_list()[0].get_votes()[1] == 2);
-		TS_ASSERT(b.get_ballot_list()[0].get_votes()[1] == 3);
+		TS_ASSERT(b.get_ballot_list()[0].get_votes()[2] == 3);
 
     }
 
-    void ReadFile_test() {
+    void test_ReadFile() {
 		BallotList b;
 		b.ReadFile("test.csv",3);
 		TS_ASSERT(b.ListSize() == 3);
@@ -99,4 +107,4 @@ class BallotListTestSuite : public CxxTest::TestSuite {
 		TS_ASSERT(b.get_ballot_list()[2].get_votes()[1] == 2);
 		TS_ASSERT(b.get_ballot_list()[2].get_votes()[2] == 3);
     }
-}
+};

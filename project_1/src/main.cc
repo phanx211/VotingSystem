41f6b5b957ada7cc8746cb83bcb8f1018e4cdeeb
Ronny Yogiswara <yogis002@umn.edu>
2018-03-21 18:00:46 -0500

    Added print to log.txt

--- project_1/src/main.cc
+++ project_1/src/main.cc
@@ -6,6 +6,7 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
+  freopen("log.txt","w",stdout);
   srand(time(NULL)); // Used for randomizing
 
   /*
@@ -49,12 +50,12 @@ int main(int argc, char *argv[]) {
     testp.ReadNames(filename); // Initialize the candidate list based on names in the csv
     init_ballots_.ReadFile(filename,atoi(argv[3])); // Initializes the list of ballots
   												// reading the lines of the csv
-    for (int i = 0; i < init_ballots_.ListSize(); i++) {
-      for (unsigned j = 0; j < init_ballots_.get_ballot_list()[i].get_votes().size(); j++ ) {
-        cout << init_ballots_.get_ballot_list()[i].get_votes()[j] << " ";
-      }
-      cout << endl;
-    }
+    // for (int i = 0; i < init_ballots_.ListSize(); i++) {
+    //   for (unsigned j = 0; j < init_ballots_.get_ballot_list()[i].get_votes().size(); j++ ) {
+    //     cout << init_ballots_.get_ballot_list()[i].get_votes()[j] << " ";
+    //   }
+    //   cout << endl;
+    // }
   // Sets the ballot_list in the Election class to init_ballots_
     testp.set_ballots(init_ballots_);
 
